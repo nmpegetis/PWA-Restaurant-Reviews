e2475481086e7297f70acc4907b0d56d6f5228ad
Nikolas Begetis <nmpegetis@gmail.com>
2018-08-25 15:53:15 +0300

    fix(sw.js): Fix suggestion #1. Caching optimising.
    Category: Application Data and Offline Use
    
    Reviewers Suggestion:
    Suggestion
    Caching resources dynamically or caching URLs during install event works but remember this is not a perfect solution. Think of the situation when the size grows up and/or has lot of URLs with dynamic query parameters !
    
    Remember the role of service worker is to serve the resource.
    Since we have all the resources required to load any detail page( restaurant.html & js files) there is no reason why we cannot load any details page offline , even if the user hasn't visited it earlier.
    
    Hint
    Please take a look at the URL in Restaurant Info screen, it's http://localhost:8000/restaurant.html?id=1 so even though you stored restaurant.html in the cache but it still hasn't found this in the cache, you should remove the query behind the /restaurant.html and it will work with every restaurant.
    
    Check out the ignoreSearch in the external resource below to get it working
    
    External Resources :books:
    https://developer.mozilla.org/en-US/docs/Web/API/Cache/match

--- sw.js
+++ sw.js
@@ -11,17 +11,17 @@ let filesToCache = [
   'dist/sw_register.bundle.js',
   'dist/sw.bundle.js',
   'css/styles.css',
-  '/restaurant.html?id=1',
-  '/restaurant.html?id=2',
-  '/restaurant.html?id=3',
-  '/restaurant.html?id=4',
-  '/restaurant.html?id=5',
-  '/restaurant.html?id=6',
-  '/restaurant.html?id=7',
-  '/restaurant.html?id=8',
-  '/restaurant.html?id=9',
-  '/restaurant.html?id=10',
-  'img/',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  'img/icon.png',
 ];
 
 this.addEventListener('install', event => {
@@ -65,7 +65,7 @@ this.addEventListener('activate', event => {
 this.addEventListener('fetch', event => {
   event.respondWith(
     caches
-      .match(event.request, { ignoreSearch: true })
+      .match(event.request, { ignoreSearch: true }) // https://developer.mozilla.org/en-US/docs/Web/API/Cache/match
       .then(response => {
         return (
           response ||
